7c03bc79faf5d6d7bd8bcfbbda8eb1bf69485f0e
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2021-11-02 19:30:20 +0100

    Created NotEnoughBatteryException

---
+++ src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
@@ -0,0 +1,30 @@
+package it.unibo.oop.lab.exception1;
+
+public class NotEnoughBatteryException extends Exception {
+
+    public NotEnoughBatteryException() {
+	// TODO Auto-generated constructor stub
+    }
+
+    public NotEnoughBatteryException(String message) {
+	super(message);
+	// TODO Auto-generated constructor stub
+    }
+
+    public NotEnoughBatteryException(Throwable cause) {
+	super(cause);
+	// TODO Auto-generated constructor stub
+    }
+
+    public NotEnoughBatteryException(String message, Throwable cause) {
+	super(message, cause);
+	// TODO Auto-generated constructor stub
+    }
+
+    public NotEnoughBatteryException(String message, Throwable cause, boolean enableSuppression,
+	    boolean writableStackTrace) {
+	super(message, cause, enableSuppression, writableStackTrace);
+	// TODO Auto-generated constructor stub
+    }
+
+}
